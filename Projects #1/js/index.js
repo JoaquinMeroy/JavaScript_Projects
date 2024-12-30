18d6404ca8e39c2160e0c650079846b350220df0
Joaquin Meroy <joaquinameroy@gmail.com>
2024-12-30 11:05:37 +0800

    Added new Projects
    Added 4 mini projects

---
+++ Projects #1/js/index.js
@@ -0,0 +1,74 @@
+// 1. Personalized Greeting App -->
+// let myH1 = document.getElementById("myH1");
+// userName = window.prompt("Enter Name");
+// myH1.textContent = `Hello ${userName}! Welcome to my app!`;
+
+
+
+// 2. Basic Calculator
+// let num1 = window.prompt("Enter first number");
+// let num2 = window.prompt("Enter Second number");
+// let operationType = prompt("Enter Operation");;
+// let myH1 = document.getElementById("myH1")
+
+// let Num1 = Number(num1);
+// let Num2 = Number(num2);
+
+// if(operationType == "+"){
+//   myH1.textContent = (Num1 + Num2);
+// }
+// else if(operationType == "-"){
+//   myH1.textContent = (Num1 - Num2);
+// }
+// else if(operationType == "*"){
+//   myH1.textContent = (Num1 * Num2);
+// }
+// else if(operationType == "/"){
+//   myH1.textContent = (Num1 / Num2);
+// }
+
+
+
+// 3. Age Calculator
+// const currentDate = 2024;
+// let myInput = document.getElementById("myInput");
+// let myLabel = document.getElementById("myLabel");
+// let myBtn = document.getElementById("myBtn");
+
+// myBtn.onclick = function(){
+//   let convertedInput = Number(myInput.value);
+
+//   let computedAge = currentDate - convertedInput;
+
+//   myLabel.textContent = `You are ${computedAge} years old!`
+// }
+
+
+
+// 4. Unit Converter
+// let myCelciusInput = document.getElementById("myCelciusInput");
+// let myCelciusBtn = document.getElementById("myCelciusBtn");
+// let myCelciusLabel = document.getElementById("myCelciusLabel");
+
+// let myFahrenheitInput = document.getElementById("myFahrenheitInput");
+// let myFahrenheitBtn = document.getElementById("myFahrenheitBtn");
+// let myFahrenheitLabel = document.getElementById("myFahrenheitLabel");
+
+// myCelciusBtn.onclick = function(){
+//   let convertedCelciusInput = Number(myCelciusInput.value);
+
+//   let convertedCelciusToFahrenheit = (convertedCelciusInput * 9/5) + 32;
+
+//   myCelciusLabel.textContent = convertedCelciusToFahrenheit;
+  
+// }
+
+// myFahrenheitBtn.onclick = function(){
+//   let convertedFahrenheitInput = Number(myFahrenheitInput.value);
+
+//   let convertedFahrenheitToCelsius = (convertedFahrenheitInput - 32) * 5/9;
+
+//   myFahrenheitLabel.textContent = convertedFahrenheitToCelsius;
+  
+// }
+
