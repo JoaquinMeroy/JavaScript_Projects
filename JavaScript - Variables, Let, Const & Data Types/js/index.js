cf0b84b566fefc242987c361bfa28519022aedd9
Joaquin Meroy <joaquinameroy@gmail.com>
2025-01-13 19:06:50 +0800

    rwret

---
+++ JavaScript - Variables, Let, Const & Data Types/js/index.js
@@ -0,0 +1,9 @@
+let username = prompt("Enter username");
+let age = prompt("Enter age");
+let favoriteColor = prompt("Enter favorite color");
+
+alert(`
+  username:${username} 
+  age:${age} 
+  favorite color:${favoriteColor}
+  `);
