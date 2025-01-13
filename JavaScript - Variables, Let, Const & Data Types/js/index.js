bf954d9e270777fd89d7dd0a40f01efdcba69340
Joaquin A. Meroy <167620611+JoaquinMeroy@users.noreply.github.com>
2025-01-13 22:51:15 +0800

    added 2 new mini projects

--- JavaScript - Variables, Let, Const & Data Types/js/index.js
+++ JavaScript - Variables, Let, Const & Data Types/js/index.js
@@ -1,9 +1,41 @@
-let username = prompt("Enter username");
-let age = prompt("Enter age");
-let favoriteColor = prompt("Enter favorite color");
+// 1. User Information Form
 
-alert(`
-  username:${username} 
-  age:${age} 
-  favorite color:${favoriteColor}
-  `);
+// let username = prompt("Enter username");
+// let age = prompt("Enter age");
+// let favoriteColor = prompt("Enter favorite color");
+
+// alert(`
+//   username:${username} 
+//   age:${age} 
+//   favorite color:${favoriteColor}
+//   `);
+
+
+
+// 2. Tip Calculator
+
+// let tip = 15;
+// let foodAmount = 200;
+// let totalAmount = foodAmount + (foodAmount * (tip/100));
+
+// let myH1 = document.getElementById("myH1");
+
+// myH1.textContent = totalAmount.toFixed(2);
+
+
+
+// 3. Daily Planner
+
+let myMorning = document.getElementById("myMorning");
+let myAfternoon = document.getElementById("myAfternoon");
+let myEvening = document.getElementById("myEvening");
+
+let Morning = prompt("Whats your morning routine");
+let Afternoon = prompt("Whats your afternon routine");
+let Evening = prompt("Whats your evening routine");
+
+myMorning.textContent = `Morning: ${Morning}`;
+myAfternoon.textContent = `Afternoon: ${Afternoon}`;
+myEvening.textContent = `Evening: ${Evening}`;
+
+
