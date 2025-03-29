a8bda90277ee6e4e140358758032d8c98d092c8f
Joaquin Meroy <joaquinameroy@gmail.com>
2025-03-29 14:24:53 +0800

    day 1
    just completed the a daily challenge for day 1

---
+++ Javascript - Challenges/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let firstName = prompt("what is your name:");
+let age = prompt("what is your age:");
+let favoriteHobby = prompt("what is your favorite hobby:");
+
+console.log(
+  `my name is ${firstName}, age is ${age} and my favorite hobby is${favoriteHobby}`
+);
